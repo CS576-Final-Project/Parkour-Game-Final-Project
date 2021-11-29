d338697f483c6550813fc7f49e4e045525796f6a
Bryan Xiao <76538460+YiquanXiao@users.noreply.github.com>
2021-11-29 05:18:32 -0500

    bullet script 3.0

--- 576 Final - rigibody/Assets/Scripts/Bullet.cs
+++ 576 Final - rigibody/Assets/Scripts/Bullet.cs
@@ -7,7 +7,10 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 1000f;
+    public float speed = 1000f;  // projectile speed
+    public GameObject explosionEffect;  // projectile explosion effect
+    // public AudioSource inFlightAudioSource;  // audio of the projectile when in flight
+    public ParticleSystem inFlightEffect;  // projectile in flight effect
 
     private float lifeTime = 5f;  // lift time of the projectile
     private float timer = 0f;  // record time 
@@ -22,6 +25,8 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // update timer
+        timer += Time.deltaTime;
         // destroy after 5 sec of not hitting anything
         if (timer > lifeTime)
         {
@@ -31,6 +36,27 @@ public class Bullet : MonoBehaviour
 
     public void OnCollisionEnter(Collision other)
     {
-        Destroy(gameObject);
+        // --- return if not enabled because OnCollision is still called if compoenent is disabled ---
+        if (!enabled) return;
+        
+        // explode when hitting an object
+        Explode();
+        // stop in flight audio
+        // inFlightAudioSource.Stop();
+        foreach(Collider col in GetComponents<Collider>())
+        {
+            col.enabled = false;
+        }
+        inFlightEffect.Stop();
+        
+        //  Destroy the projectile after 2 seconds. Using a delay because the particle system needs to finish
+        Destroy(gameObject, 5f);
     }
+    
+    private void Explode()
+    {
+        // explosion
+        Instantiate(explosionEffect, transform.position, explosionEffect.transform.rotation, null);
+    }
+    
 }
