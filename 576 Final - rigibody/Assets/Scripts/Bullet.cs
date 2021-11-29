e88c6739925e283d6df766542361d0d6c8b9717f
Bryan Xiao <76538460+YiquanXiao@users.noreply.github.com>
2021-11-29 04:43:36 -0500

    bullet script 2.0

--- 576 Final - rigibody/Assets/Scripts/Bullet.cs
+++ 576 Final - rigibody/Assets/Scripts/Bullet.cs
@@ -8,6 +8,10 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 1000f;
+
+    private float lifeTime = 5f;  // lift time of the projectile
+    private float timer = 0f;  // record time 
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +22,11 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // destroy after 5 sec of not hitting anything
+        if (timer > lifeTime)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void OnCollisionEnter(Collision other)
