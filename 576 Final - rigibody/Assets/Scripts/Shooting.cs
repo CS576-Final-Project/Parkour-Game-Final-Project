48adba25e4d9c13421ebd557e79ed23fa37ef16c
Bryan Xiao <76538460+YiquanXiao@users.noreply.github.com>
2021-11-29 02:53:42 -0500

    simple bullet implementation & test in shooting

--- 576 Final - rigibody/Assets/Scripts/Shooting.cs
+++ 576 Final - rigibody/Assets/Scripts/Shooting.cs
@@ -19,6 +19,9 @@ public class Shooting : MonoBehaviour
     // gun tip
     public Transform gunTip;
     
+    // TODO: delete
+    public GameObject bullet;
+    
     // fire rate
     private float cd = 0.2f;
     // timer 
@@ -42,6 +45,8 @@ public class Shooting : MonoBehaviour
             // fire effect at gun tip
             Instantiate(fireEffect, gunTip);
             Shoot();
+            // TODO: delete
+            Instantiate(bullet, gunTip.position, gunTip.rotation);
         }
     }
 
